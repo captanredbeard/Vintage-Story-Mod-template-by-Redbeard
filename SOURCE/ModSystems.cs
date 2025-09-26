14f164a7ca5e82969eb8ef4b783dab2b06f322d6
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-09-26 14:00:04 -0500

    Update ModSystems.cs
    some main class pre setup for the template

--- SOURCE/ModSystems.cs
+++ SOURCE/ModSystems.cs
@@ -4,5 +4,17 @@ namespace $projectname$;
 
 public sealed class $projectname$ModSystem : ModSystem
 {
-
+  internal string ModID = "$projectname$";
+  internal static ICoreAPI? Api;
+  
+  public override void Start(ICoreAPI api)
+  {
+    Api = api;
+    ModID = Mod.Info.ModID;
+    base.Start(api);
+  }
+  public override void AssetsLoaded(ICoreAPI api)
+  {
+    base.AssetsLoaded(api);
+  }
 }
